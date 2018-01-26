ccdfaf6b83f81a56b81a79931308ccfaaf61db01
liyu <13798464518@163.com>
2018-01-26 14:15:25 +0800

    Initial Commit

---
+++ ScaryBugsDemo/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ScaryBugsDemo
+//
+//  Created by liyu on 2018/1/26.
+//  Copyright © 2018年 liyu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
